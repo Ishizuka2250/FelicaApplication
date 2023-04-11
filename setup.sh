006f41795ff8c0d563e4cff6099868c0497681cc
Ishizuka2250 <squid1028@gmail.com>
2023-04-11 19:15:01 +0900

    [Create] Setup.sh LinuxでFelicaを利用する際のセットアップスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin
+# For use the Felica RCS-380 setup.
+sudo apt-get install libusb-dev python3-usb
+pip install nfcpy
+sudo adduser <USERNAME> plugdev
+sudo sh -c 'echo SUBSYSTEM==\"usb\", ACTION==\"add\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"06c1\", GROUP=\"plugdev\" >> /etc/udev/rules.d/nfcdev.rules'
+sudo udevadm control -R
