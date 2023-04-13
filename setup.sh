02cd578be1d28a8b25368129b3ecccaa91382e88
Ishizuka2250 <squid1028@gmail.com>
2023-04-13 18:45:52 +0900

    [Modify] setup.sh - pip requests を追加

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # For use the Felica RCS-380 setup.
 sudo apt-get install libusb-dev python3-usb
 pip install nfcpy
+pip install requests
 sudo adduser <USERNAME> plugdev
 sudo sh -c 'echo SUBSYSTEM==\"usb\", ACTION==\"add\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"06c1\", GROUP=\"plugdev\" >> /etc/udev/rules.d/nfcdev.rules'
 sudo udevadm control -R
