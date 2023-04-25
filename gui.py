38a426ab29e41be277299522ddfea176d79d6cfc
Ishizuka2250 <squid1028@gmail.com>
2023-04-25 23:31:20 +0900

    [Feature] エラーメッセージ表示機能の実装 - エラーメッセージファイル(ERROR_MESSAGE)を追加 - request実行時の例外処理を追加

--- gui.py
+++ gui.py
@@ -10,13 +10,14 @@ class GUI:
     # GUIの部品配置
     self.__rootWindow = tk.Tk()
     # 全画面表示
-    self.__rootWindow.attributes("-fullscreen", True)
+    #self.__rootWindow.attributes("-fullscreen", True)
+    self.__rootWindow.geometry("1024x600")
     # Escキーでアプリケーション終了
     self.__rootWindow.bind("<Escape>", self.__applicationTerminate)
 
     self.__mainFrame = tk.Frame(self.__rootWindow)
     self.__mainFrame.pack(expand=True, anchor=tk.CENTER)
-    self.__label = tk.Label(self.__mainFrame, text="Ready.", font=("", "40", ""))
+    self.__label = tk.Label(self.__mainFrame, text="Ready.", font=("", "30", ""))
     self.__label.pack(expand=True, anchor=tk.CENTER)
 
     # アプリケーションモード取得
@@ -24,7 +25,7 @@ class GUI:
     self.__AppMode = dotenv.get_key(".env", "APP_MODE")
     self.__request = rs.RequestServer()
     if not self.__request.sessionCreate():
-      msgbox.showerror("error","サーバーへのログインに失敗しました.")
+      msgbox.showerror("Error","サーバーへのログインに失敗しました.")
       sys.exit(1)
     
     self.__felica = fc.FelicaControl(self, self.__request)
@@ -40,16 +41,18 @@ class GUI:
     self.__rootWindow.mainloop()
 
   def changeLabel(self, requestResult) -> None:
-    if requestResult == 0 and self.__AppMode == "ISSUE":
-      text = "順番待ち番号の登録が完了しました."
-    elif requestResult == 0 and self.__AppMode == "UPDATE":
-      text = "カット中に更新しました."
-    elif requestResult == 4 or requestResult == 8:
-      text = "このカードは既にタッチされています."
-    else:
-      text = "エラーにより失敗しました."
-    
     # ラベルを text の値に変更
+    text = ''
+    print(requestResult)
+    if (len(requestResult) > 0):
+      text = dotenv.get_key("ERROR_MESSAGE", requestResult)
+    else:
+      if self.__AppMode == "ISSUE":
+        requestResult = "P0001"
+      elif self.__AppMode == "UPDATE":
+        requestResult = "P0002"
+
+    text = dotenv.get_key("ERROR_MESSAGE", requestResult)
     self.__label["text"] = text
 
 
--- requestServer.py
+++ requestServer.py
@@ -22,7 +22,9 @@ class RequestServer():
       sys.exit(1)
 
   def sessionCreate(self) -> bool:
+    # AccessTokenが有効かチェック
     if not self.__authCheck():
+      # AccessTokenが無効 -> ログイン実行
       if not self.__login():
         return False
     return True
@@ -33,18 +35,22 @@ class RequestServer():
     elif self.__AppMode == 'UPDATE':
       return self.__cutNowWaitNumber(idm)
     else:
-      return 10
+      return "P0101"
 
   def __login(self) -> bool:
     targetURL = self.__URL + "/api/v1/auth/login"
     # loginAPIへリクエスト
-    response = requests.post(
-      url=targetURL,
-      params={
-        "email": self.__MailAddress,
-        "password": self.__Password
-      })
-    
+    try:
+      response = requests.post(
+        url=targetURL,
+        params={
+          "email": self.__MailAddress,
+          "password": self.__Password
+        })
+    except Exception as e:
+      print("Error:Failed to login request.")
+      print(e)
+      return False
     # ログイン成功 -> True
     if response.status_code == 200:
       print("Info:Login success.")
@@ -62,14 +68,18 @@ class RequestServer():
     # .envにAccesstokenが記載されているかチェック
     if accessToken is not None and len(accessToken) > 0:
       # AccessTokenチェックAPIにリクエスト
-      response = requests.get(
-        url=targetURL,
-        headers={
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          "Authorization": "Bearer " + accessToken
-        })
-      
+      try:
+        response = requests.get(
+          url=targetURL,
+          headers={
+            "Content-Type": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "Authorization": "Bearer " + accessToken
+          })
+      except Exception as e:
+        print("Failed to authentication request.")
+        print(e)
+        return False
       # Accesstokenが有効 -> True
       if response.status_code == 200:
         print("Info:Authentication success.")
@@ -83,73 +93,84 @@ class RequestServer():
       print("Warn:.env file has not The AccessToken. Please try login.")
       return False
   
-  def __issueWaitNumber(self, idm) -> int:
+  def __issueWaitNumber(self, idm) -> str:
     targetURL = self.__URL + "/api/v1/waiting"
     accessToken = dotenv.get_key(".env", "ACCESSTOKEN")
     # .envにAccesstokenが記載されているかチェック
     if accessToken is not None and len(accessToken) > 0:
       # 待ち番号発行APIにリクエスト
-      response = requests.post(
-        url=targetURL,
-        headers={
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          "Authorization": "Bearer " + accessToken
-        },
-        params={
-          "key": idm
-        })
+      try:
+        response = requests.post(
+          url=targetURL,
+          headers={
+            "Content-Type": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "Authorization": "Bearer " + accessToken
+          },
+          params={
+            "key": idm
+          })
+      except Exception as e:
+        # リクエストに失敗した場合
+        print("Error:Failed to issue request.")
+        print(e)
+        return "P0102"
       
       # APIの実行が正常に行われたか？
       if response.status_code != 201:
-        # 待ち番号の発行に失敗した場合 -> errorCode:1～6
+        # 待ち番号の発行に失敗した場合 -> errorCode:A0301～A0306
         print(response.json()["message"])
         return response.json()["errorcode"]
     else:
-      # Accesstokenが保存されていない場合 -> errorCode:9
-      print("Warn:.env file has not The AccessToken. Please try login.")
-      return 9
+      # Accesstokenが保存されていない場合 -> errorCode:P0202
+      print("Error:.env file has not The AccessToken. Please try login.")
+      return "P0103"
     print("Info:" + response.json()["message"])
-    return 0
+    return ''
   
-  def __cutNowWaitNumber(self, idm) -> int:
+  def __cutNowWaitNumber(self, idm) -> str:
     targetURL = self.__URL + "/api/v1/waiting"
     accessToken = dotenv.get_key(".env", "ACCESSTOKEN")
     
     # .envにAccesstokenが記載されているかチェック
     if accessToken is not None and len(accessToken) > 0:
       # 待ち番号更新APIにリクエスト
-      response = requests.patch(
-        url=targetURL,
-        headers={
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          "Authorization": "Bearer " + accessToken
-        },
-        params={
-          "key": idm
-        })
+      try:
+        response = requests.patch(
+          url=targetURL,
+          headers={
+            "Content-Type": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "Authorization": "Bearer " + accessToken
+          },
+          params={
+            "key": idm
+          })
+      except Exception as e:
+        print("Error:Failed to update (cut now state change) request.")
+        print(e)
+        return "P0104"
       # 待ち番号の更新が正常に行われたか？
       if response.status_code != 200:
-        # 待ち番号の更新に失敗した場合 -> errorCode:1～8
+        # 待ち番号の更新に失敗した場合 -> errorCode:A0307～A0315
         print(response.json()["message"])
         return response.json()["errorcode"]
     else:
-      # Accesstokenが保存されていない場合 -> errorCode:9
+      # Accesstokenが保存されていない場合 -> errorCode:P0103
       print("Warn:.env file has not The AccessToken. Please try login.")
-      return 9
+      return "P0103"
     print("Info:" + response.json()["message"])
-    return 0
+    return ''
   
   def cutDoneWaitNumber(self, waitNumber) -> int:
     targetURL = self.__URL + "/api/v1/waiting"
     accessToken = dotenv.get_key(".env", "ACCESSTOKEN")
     masterKey = dotenv.get_key(".env", "MASTER_KEY")
     waitNumberID = self.__getWaitNumberID(waitNumber)
-    # タッチされたカード(カット中)の待ち番号が発行されていない場合 -> errorCode:10
+    # タッチされたカード(カット中)の待ち番号が発行されていない場合 -> errorCode:P0105
     if waitNumberID == 0:
       print("Error:The card number touched has not been issued.")
-      return 10
+      return "P0105"
     updateStatus = {
       "waiting_numbers": [{
         "id": waitNumberID,
@@ -157,35 +178,38 @@ class RequestServer():
         "is_cut_done": 1,
         "is_cut_call": 0,
         "is_cut_now": 0
-      }]
-    }
+      }]}
     
     # .envにAccesstokenが記載されているかチェック
     if accessToken is not None and len(accessToken) > 0:
       # 待ち番号更新APIにリクエスト
-      response = requests.patch(
-        url=targetURL,
-        headers={
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          "Authorization": "Bearer " + accessToken
-        },
-        params={
-          "key": masterKey
-        },
-        data=json.dumps(updateStatus)
-        )
+      try:
+        response = requests.patch(
+          url=targetURL,
+          headers={
+            "Content-Type": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "Authorization": "Bearer " + accessToken
+          },
+          params={
+            "key": masterKey
+          },
+          data=json.dumps(updateStatus))
+      except Exception as e:
+        print("Error:Failed to update (cut done state change) request.")
+        print(e)
+        return "P0106"
       # 待ち番号の更新が正常に行われたか？
       if response.status_code != 200:
-        # 待ち番号の更新に失敗した場合 -> errorCode:1～8
+        # 待ち番号の更新に失敗した場合 -> errorCode:A0307～A0315
         print(response.json()["message"])
         return response.json()["errorcode"]
     else:
-      # Accesstokenが保存されていない場合 -> errorCode:9
+      # Accesstokenが保存されていない場合 -> errorCode:P0103
       print("Warn:.env file has not The AccessToken. Please try login.")
-      return 9
+      return "P0103"
     print("Info:" + response.json()["message"])
-    return 0
+    return ''
 
   def __getWaitNumberID(self, waitNumber) -> int:
     waitNumberDictionaries = self.getWaitNumbers()
@@ -197,7 +221,12 @@ class RequestServer():
   
   def getWaitNumbers(self) -> dict:
     targetURL = self.__URL + "/api/v1/waiting"
-    response = requests.get(url=targetURL)
+    try:
+      response = requests.get(url=targetURL)
+    except Exception as e:
+      print("Error:Failed to get the Wait Numbers request.")
+      print(e)
+      return None
     return response.json()["wait_number"]
     
 
